6c42597a45452704e162052f246a736b0d4e1ead
thiagomedeiros11 <thiago.wav1@gmail.com>
2024-08-28 14:44:37 -0300

    feat: update highscores after 50 seconds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { HighscoresService, Character } from './services/highscores.service';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,27 +19,47 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   highscores: Character[] =[];
+  private interevalId: any;
+  private subscription: Subscription = new Subscription();
 
   constructor(private highscoresService: HighscoresService) {}
 
   ngOnInit() {
-
     const storedHighscores = localStorage.getItem('highscores');
+
     if (storedHighscores) {
       this.highscores = JSON.parse(storedHighscores);
     }
 
-    this.highscoresService.getHighscores().subscribe(
+    this.fetchHighscores();
+
+    this.interevalId = setInterval(() => {
+      this.fetchHighscores();
+    }, 50000);
+  }
+
+  fetchHighscores() {
+    const subscription = this.highscoresService.getHighscores().subscribe(
       (data: Character[]) => {
         this.highscores = data;
+
         localStorage.setItem('highscores', JSON.stringify(data));
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
+
+    this.subscription.add(subscription);
+  }
+
+  ngOnDestroy() {
+      if (this.interevalId) {
+        clearInterval(this.interevalId);
+      }
+      this.subscription.unsubscribe();
   }
 }
